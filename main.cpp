5477002fc9683a4d6ce1538ef8d55dbe395c6a27
FayejAhamed <foyezfoyez017@gmail.com>
2025-05-02 02:36:54 -0400

    C++ Tampletes class practice

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Person {
+private:
+	string name;
+public:
+	Person(string n) :name(n) {}
+	string getName() { return name; }
+};
+template <class T>
+T largest(T& one, T& two, T& three) {
+	T max = one;
+	if (max < two)
+		max = two;
+	if (max < three)
+		max = three;
+	return max;
+}// endswap
+
+int main() {
+	/*int first = 3;
+	int second = 5;
+	int third = 6;*/
+	char first = 'a';
+	char second = 'B';
+	char third = 'A';
+	cout << "Vslues are =: " << first << " " << second << " " << third << endl;
+	cout << "The largest values is: " << largest(first, second, third) << endl;
+
+	return 0;
+}
